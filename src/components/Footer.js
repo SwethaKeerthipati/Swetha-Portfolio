e145090cf7fb46cc113d83384b3c57a3f0bbec17
swethakeerthipati <swethakeerthipati@gmail.com>
2023-08-17 02:50:14 +0200

    Modify Responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,7 @@ const Footer = () => {
   return (
     <div className="footer bg-blue-200 flex flex-col gap-2 items-center justify-center p-5 mt-9">
       <div className="sm:text-sm">
-        Copyright &copy; <span>2023 Swetha Keerthipati</span> All rights
-        reserved
+        Copyright &copy; <span>2023 Swetha Keerthipati</span>
       </div>
     </div>
   );
